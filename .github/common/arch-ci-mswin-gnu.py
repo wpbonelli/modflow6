660aede4f88ef743f82feec896bae10315ef2840
Wes Bonelli <wpb3hw@virginia.edu>
2023-03-09 19:22:26 -0500

    add custom python petsc configure script

---
+++ .github/common/arch-ci-mswin-gnu.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+configure_options = [
+  '--with-mpiexec=\'/C/Program\ Files/Microsoft\ MPI/Bin/mpiexec\'',
+  '--with-shared-libraries=0',
+  '--with-batch=1',
+  '--with-debugging=0',
+  # not using -g so that the binaries are smaller
+  'COPTFLAGS=-O',
+  'FOPTFLAGS=-O',
+  'CXXOPTFLAGS=-O',
+  '--with-visibility=0',
+  'FFLAGS=-fno-backtrace -ffree-line-length-0',
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
