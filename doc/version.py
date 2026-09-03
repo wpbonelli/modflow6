19a93386aeffd86de44bfb46924892c5fa5bb3d2
wpbonelli <wpbonelli@gmail.com>
2026-09-03 10:19:42 -0400

    chore(release): update version strings

--- doc/version.py
+++ doc/version.py
@@ -1,3 +1,3 @@
 # MODFLOW 6 version file automatically created using...update_version.py
-# created on...September 02, 2026 21:50:01
-__version__ = "6.8.0"
+# created on...September 03, 2026 10:18:46
+__version__ = "6.9.0.dev0"
